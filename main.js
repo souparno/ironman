56b4a83a33f0ce5d06eb19a3b56796d808939522
souparno majumder <souparno.majumder@gmail.com>
2014-07-08 15:24:39 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,79 @@
+function Core(){
+
+this.Create=function(o){
+    var F=function (){};
+    F.Extend=this.Extend;
+    for(var property in o){
+        F.prototype[property]=o[property];    
+    }
+    return F;
+};
+
+this.Extend=function(o){
+  for(var property in o){
+      this.prototype[property]=o[property];    
+  }
+  return this;
+};
+};
+var Class=new Core();
+
+// How to use this library
+
+
+var Person=Class.Create({
+walk:function(){
+console.log("I am Walking");
+},
+
+sayHello:function(){
+    console.log("Hello there");
+}
+});
+
+var Student=Person.Extend({
+sayHello:function(){
+console.log("Hi i am a student");
+
+},
+
+sayGoodBye:function(){
+   console.log("Good Bye");
+}        
+});
+
+var HiSchoolStudent=Student.Extend({
+study:function(){
+console.log("I study in high school");
+}
+});
+
+var Vehicle = Class.Create({
+run:function(){
+console.log("I am a vehicle and I run");
+}
+});
+
+var Truck = Vehicle.Extend({
+init: function(hp, wheels) {
+this.horsepower = hp;
+this.wheels = wheels;
+},
+
+printInfo: function() {
+console.log('I am a truck and I have ' + this.horsepower + ' hp. and '+this.wheels +' wheels');
+}
+});
+
+var student1 = new HiSchoolStudent();
+student1.study();
+student1.sayHello();
+student1.walk();
+student1.sayGoodBye();
+
+var obj=new Truck();
+obj.run();
+obj.init();
+obj.printInfo(350,4);
+
+console.log(obj instanceof Vehicle);
