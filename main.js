d07b28c0ee14e1651db6443367577eb492b16963
souparno majumder <souparno.majumder@gmail.com>
2014-07-14 14:29:02 +0530

    init

--- main.js
+++ main.js
@@ -13,72 +13,36 @@ var Class = {
     },
             
     Extend: function(o) {
+        var F=function(){            
+        };
+        F.Extend=this.Extend;
+        F.prototype['super']=new this;
         for (var property in o) {
-            this.prototype[property] = o[property];
-        }
-        return this;
+            F.prototype[property] = o[property];
+        }        
+        return F;
     }
 };
 
 
-// How to use this library
-
-
-var Person = Class.Create({
-    walk: function() {
-        console.log("I am Walking");
-    },
-    sayHello: function() {
-        console.log("Hello there");
+var MusicInstrument=Class.Create({
+    
+    sayHello:function(){
+        console.log("I am a musical instrument");
     }
+    
 });
 
 
-var Student = Person.Extend({
-    sayHi: function() {
-        console.log("Hi i am a student");
-
-    },
-    sayGoodBye: function() {
-        console.log("Good Bye");
+var Guitar=MusicInstrument.Extend({
+    
+    sayHello:function(){
+        this.super.sayHello();
+        console.log("Hello i am a guitar");
     }
+    
 });
 
 
-
-var HiSchoolStudent = Student.Extend({
-    study: function() {
-        console.log("I study in high school");
-    }
-});
-
-var Vehicle = Class.Create({
-    run: function() {
-        console.log("I am a vehicle and I run");
-    }
-});
-
-var Truck = Vehicle.Extend({
-    init: function(hp, wheels) {
-        this.horsepower = hp;
-        this.wheels = wheels;
-    },
-    printInfo: function() {
-        console.log('I am a truck and I have ' + this.horsepower + ' hp. and ' + this.wheels + ' wheels');
-    }
-});
-
-var student1 = new HiSchoolStudent();
-student1.sayHi();
-student1.study();
-student1.sayHello();
-student1.walk();
-student1.sayGoodBye();
-
-var obj = new Truck();
-obj.run();
-obj.init();
-obj.printInfo(350, 4);
-
-console.log(obj instanceof Person);
-console.log(obj instanceof Vehicle);
+var guitar=new Guitar();
+guitar.sayHello();
