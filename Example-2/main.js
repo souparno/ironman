e1dcfef25b1570dacbe9eaee11949ad942f41523
souparno <souparno.majumder@gmail.com>
2015-11-10 22:03:57 +0530

    another set of javascript class stucture example created and old one moved to the Example-1 file

---
+++ Example-2/main.js
@@ -0,0 +1,93 @@
+var isArray = function (obj) {
+    return Object.prototype.toString.call(obj) === '[object Array]';
+};
+
+var __class__ = function (cls, name, parent, proto) {
+    var clsProto = function () {};
+    var logger;
+
+    if (typeof name !== 'string') {
+        proto = parent;
+        parent = name;
+        name = null;
+    }
+
+    if (name) {
+        logger = exports.logging.get(name);
+    }
+
+    if (!parent) {
+        throw new Error('parent or prototype not provided');
+    }
+    if (!proto) {
+        proto = parent;
+        parent = null;
+    }
+
+    if (parent) {
+        if (isArray(parent)) { // multiple inheritance, use at your own risk =)
+            clsProto.prototype = {};
+            for (var i = 0, p; p = parent[i]; ++i) {
+                if (p == Error && ErrorParentClass) {
+                    p = ErrorParentClass;
+                }
+                for (var item in p.prototype) {
+                    if (!(item in clsProto.prototype)) {
+                        clsProto.prototype[item] = p.prototype[item];
+                    }
+                }
+            }
+            parent = parent[0];
+        } else {
+            if (parent == Error && ErrorParentClass) {
+                parent = ErrorParentClass;
+            }
+            clsProto.prototype = parent.prototype;
+        }
+    }
+
+    var supr = parent ? function (context, method, args) {
+        var f = parent.prototype[method];
+        if (!f) {
+            throw new Error('method ' + method + ' does not exist');
+        }
+        return f.apply(context, args || []);
+    } : null;
+
+    var p = cls.prototype = new clsProto();
+    p.constructor = cls;
+    p.__parentClass__ = parent;
+    if (name) {
+        p.__class__ = name;
+    }
+    proto.call(p, logger || supr, logger && supr);
+    return cls;
+};
+
+var Class = function (name, parent, proto) {
+    return __class__(function () {
+        return this.init && this.init.apply(this, arguments);
+    }, name, parent, proto);
+};
+
+var Vehicle = Class(function () {
+    this.init = function (wheels) {
+        this.wheels = wheels;
+    };
+});
+
+var Truck = Class(Vehicle, function (supr) {
+
+    this.init = function (hp, wheels) {
+        supr(this, "init", [wheels]);
+        this.horsepower = hp;
+    };
+
+    this.printInfo = function () {
+        console.log('I am a truck and I have ' + this.wheels +
+                ' wheels and ' + this.horsepower + ' hp.');
+    };
+});
+
+var t = new Truck(350, 4);
+t.printInfo();
