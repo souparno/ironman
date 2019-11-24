e71e5f2fc7df1f9a8fa1134b14c6290b16062174
souparno <souparno.majumder@gmail.com>
2019-11-24 15:38:32 +0530

    creating a function called createProp to create or inherit the property of the base class from the superclass

--- src/class.js
+++ src/class.js
@@ -1,8 +1,8 @@
-var Class = (function () {
+var Class = (function() {
     'use strict';
 
     var util = {
-        isFunc: function (arr) {
+        isFunc: function(arr) {
             var key;
 
             for (key in arr) {
@@ -12,9 +12,10 @@ var Class = (function () {
             }
             return true;
         },
-        overrideFunc: function (super_fn, base_fn) {
-            return function () {
-                var tmp = this._super, ret;
+        overrideFunc: function(super_fn, base_fn) {
+            return function() {
+                var tmp = this._super,
+                    ret;
 
                 this._super = super_fn;
                 ret = base_fn.apply(this, arguments);
@@ -22,35 +23,35 @@ var Class = (function () {
                 return ret;
             };
         },
-        mergeObj: function (super_obj, base_obj) {
-            var property, func1, func2;
-
-            for (property in base_obj) {
-
-                func1 = super_obj[property];
-                func2 = base_obj[property];
-
-                if (this.isFunc([func1, func2])) {
-                    super_obj[property] = this.overrideFunc(func1, func2);
-                } else {
-                    super_obj[property] = func2;
-                }
+        createProp: function(supr_prop, base_prop) {
+            if (this.isFunc([supr_prop, base_prop])) {
+                return this.overrideFunc(supr_prop, base_prop);
             }
-            return super_obj;
+
+            return base_prop;
+        },
+        mergeObj: function(supr_obj, base_obj) {
+            for (var prop in base_obj) {
+                supr_obj[prop] = this.createProp(supr_obj[prop], base_obj[prop]);
+
+            }
+
+            return supr_obj;
         }
     };
 
     return {
         __extending: false,
-        Create: function (o) {
-            return this.Extend.call(function () {}, o);
+        Create: function(o) {
+            return this.Extend.call(function() {}, o);
         },
-        Extend: function (base_obj) {
-            var F = function () {
-                if (!Class.__extending && this.init) {
-                    this.init.apply(this, arguments);
-                }
-            }, super_obj;
+        Extend: function(base_obj) {
+            var F = function() {
+                    if (!Class.__extending && this.init) {
+                        this.init.apply(this, arguments);
+                    }
+                },
+                super_obj;
 
             Class.__extending = true;
             super_obj = new this();
